13844d8f508a5a71a4217bc3a4144742949c5de2
lepusDZ <you@example.com>
2023-05-27 22:55:13 -0400

    Login and Logout (+ Fixed error messages)

--- TheProject/User/urls.py
+++ TheProject/User/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('register/', register, name = 'register'),
-    path('login/', login, name = 'login'),
+    path('register/', register_request, name = 'register'),
+    path('login/', login_request, name = 'login'),
+    path('logout/', logout_request, name = 'logout')
 ]
--- TheProject/User/views.py
+++ TheProject/User/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import *
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
-def register(request):
+def register_request(request):
 	if request.method == "POST":
 		form = NewUserForm(request.POST)
 		if form.is_valid():
@@ -17,4 +18,26 @@ def register(request):
 	return render(request=request, template_name="user/register.html", context={"register_form":form})
 
 
-# дописать def login(), разобраться с messages
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:home")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="user/login.html", context={"login_form":form})
+
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("main:home")
