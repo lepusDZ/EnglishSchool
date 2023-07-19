addd0d27c9dcac70f7d104aaeb7932995e55581d
lepusDZ <you@example.com>
2023-07-18 22:45:18 -0400

    Manage models in admin

--- TheProject/Main/admin.py
+++ TheProject/Main/admin.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-from django.contrib import admin
+@admin.register(Main)
+class MainAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+    ordering = ('title',)
 
-# from .models import News, Category
-
-# class NewsAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
-#     list_display_links = ('id', 'title')
-#     search_fields = ('title', 'content')
-#     list_editable = ('is_published', 'category')
-#     list_filter = ('is_published', 'category')
-
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'title')
-#     search_fields = ('title',)
-
-# admin.site.register(News, NewsAdmin)
-# admin.site.register(Category, CategoryAdmin) 
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'photo',)
