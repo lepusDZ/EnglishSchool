1eb52b542fb75aea4eb1ee0c93d991b5fe247d84
lepusDZ <you@example.com>
2023-09-28 23:28:38 -0400

    email fix, schedule bug fix

--- TheProject/TheProject/settings.py
+++ TheProject/TheProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -144,3 +145,12 @@ AUTH_USER_MODEL = 'User.CustomUser'
 LANGUAGES = [
     ('uk', 'Ukrainian'),
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'emailgarant2@gmail.com'
+EMAIL_HOST_PASSWORD = 'gzagylmcpczidsun'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
