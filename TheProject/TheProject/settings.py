206ae2497943488eea04a91f2dce3a50304b486a
lepusDZ <emailgarant@gmail.com>
2023-10-29 01:10:20 -0400

    Deployment

--- TheProject/TheProject/settings.py
+++ TheProject/TheProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+d8r(24@i#vzux!@1nql_kfn28wm!&$^vvm*pky9ea1)17o-%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
